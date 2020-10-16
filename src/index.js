395540a9d32de2a85588f1d0492e4f540d38e697
jorgebush11 <70361438+jorgebush11@users.noreply.github.com>
2020-10-16 17:01:33 -0500

    array operations function

--- src/index.js
+++ src/index.js
@@ -42,6 +42,7 @@ const StarMatch = () => {
 				<div className="stars">{elems}</div>
 				<div className="nums">{nums}</div>
 			</div>
+			<div className="timer">Time remaining: 10s</div>
 		</div>
 	);
 };
@@ -51,6 +52,35 @@ const colors = {
 	used: "lightgreen",
 	wrong: "lightcoral",
 	candidate: "deepskyblue"
+};
+
+const utils = {
+	// Sum an array
+	sum: (arr) => arr.reduce((acc, curr) => acc + curr, 0),
+
+	// create an array of numbers between min and max (edges included)
+	range: (min, max) => Array.from({ length: max - min + 1 }, (_, i) => min + i),
+
+	// pick a random number between min and max (edges included)
+	random: (min, max) => min + Math.floor(Math.random() * (max - min + 1)),
+
+	// Given an array of numbers and a max...
+	// Pick a random sum (< max) from the set of all available sums in arr
+	randomSumIn: (arr, max) => {
+		const sets = [[]];
+		const sums = [];
+		for (let i = 0; i < arr.length; i++) {
+			for (let j = 0, len = sets.length; j < len; j++) {
+				const candidateSet = sets[j].concat(arr[i]);
+				const candidateSum = utils.sum(candidateSet);
+				if (candidateSum <= max) {
+					sets.push(candidateSet);
+					sums.push(candidateSum);
+				}
+			}
+		}
+		return sums[utils.random(0, sums.length - 1)];
+	}
 };
 
 ReactDom.render(<StarMatch />, root);
