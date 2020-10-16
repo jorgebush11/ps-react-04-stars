320c9ade7925955dc6916a270d33fdd6bc1fee96
jorgebush11 <70361438+jorgebush11@users.noreply.github.com>
2020-10-16 16:33:57 -0500

    game styles midway

--- src/index.js
+++ src/index.js
@@ -5,4 +5,52 @@ import "./styles.scss";
 
 const root = document.querySelector("#root");
 
-ReactDom.render(React.createElement("h1", {}, "Stars Game"), root);
+function addStars() {
+	const num = 9;
+
+	const elems = [];
+	for (let i = 0; i < num; i++) {
+		elems.push(<div className="star" key={i}></div>);
+	}
+
+	return elems;
+}
+
+function addNums() {
+	const num = 9;
+
+	const nums = [];
+	for (let i = 0; i < num; i++) {
+		nums.push(
+			<div className="num" key={i}>
+				{i + 1}
+			</div>
+		);
+	}
+
+	return nums;
+}
+
+const StarMatch = () => {
+	const elems = addStars();
+	const nums = addNums();
+
+	return (
+		<div className="container">
+			<p>Pick 1 or more numbers that sum to the number of stars</p>
+			<div className="box">
+				<div className="stars">{elems}</div>
+				<div className="nums">{nums}</div>
+			</div>
+		</div>
+	);
+};
+
+const colors = {
+	available: "lightgray",
+	used: "lightgreen",
+	wrong: "lightcoral",
+	candidate: "deepskyblue"
+};
+
+ReactDom.render(<StarMatch />, root);
