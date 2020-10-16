3ac5ed56011af7b90ee44954d3a2abe59eb0dc32
jorgebush11 <70361438+jorgebush11@users.noreply.github.com>
2020-10-16 17:18:45 -0500

    stars number dynamic

--- src/index.js
+++ src/index.js
@@ -5,42 +5,25 @@ import "./styles.scss";
 
 const root = document.querySelector("#root");
 
-function addStars() {
-	const num = 9;
-
-	const elems = [];
-	for (let i = 0; i < num; i++) {
-		elems.push(<div className="star" key={i}></div>);
-	}
-
-	return elems;
-}
-
-function addNums() {
-	const num = 9;
-
-	const nums = [];
-	for (let i = 0; i < num; i++) {
-		nums.push(
-			<div className="num" key={i}>
-				{i + 1}
-			</div>
-		);
-	}
-
-	return nums;
-}
-
 const StarMatch = () => {
-	const elems = addStars();
-	const nums = addNums();
+	const stars = utils.random(1, 9);
 
 	return (
 		<div className="container">
 			<p>Pick 1 or more numbers that sum to the number of stars</p>
 			<div className="box">
-				<div className="stars">{elems}</div>
-				<div className="nums">{nums}</div>
+				<div className="stars">
+					{utils.range(1, stars).map((starId) => (
+						<div key={starId} className="star"></div>
+					))}
+				</div>
+				<div className="nums">
+					{utils.range(1, 9).map((number) => (
+						<button key={number} className="num">
+							{number}
+						</button>
+					))}
+				</div>
 			</div>
 			<div className="timer">Time remaining: 10s</div>
 		</div>
