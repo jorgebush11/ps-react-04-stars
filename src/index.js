69817d02a8bc626f18f10d57ce0dde3e4b5ec32c
jorgebush11 <70361438+jorgebush11@users.noreply.github.com>
2020-10-17 08:50:03 -0500

    check and uncheck buttons ready

--- src/index.js
+++ src/index.js
@@ -50,7 +50,11 @@ const StarMatch = () => {
 			return;
 		}
 
-		const newCandidateNums = candidateNums.concat(number);
+		const newCandidateNums =
+			currentStatus === "available"
+				? candidateNums.concat(number)
+				: candidateNums.filter((cn) => cn !== number);
+
 		if (utils.sum(newCandidateNums) !== stars) {
 			setCandidateNums(newCandidateNums);
 		} else {
